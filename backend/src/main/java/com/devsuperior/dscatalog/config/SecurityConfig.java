ee9ee3df3f2ac065ff0f1d1a36074c333eaf1c68
Alexandre Andrade <alex_dev@outlook.com.br>
2023-02-17 05:06:08 -0300

    User CRUD

---
+++ backend/src/main/java/com/devsuperior/dscatalog/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.devsuperior.dscatalog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	SecurityFilterChain web(HttpSecurity http) throws Exception  {
+		http.authorizeHttpRequests().anyRequest().permitAll().and().csrf().disable();
+		return http.build();
+
+	}
+}
